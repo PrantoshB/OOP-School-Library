e6d2a548a12273c21af53c41f88cc67f453bf037
gauravgangwarweb <gauravgangwar501@gmail.com>
2023-03-15 18:14:33 +0530

    Modify book rb file with hash method

--- book.rb
+++ book.rb
@@ -10,4 +10,12 @@ class Book
   end
 
   attr_accessor :title, :author, :rentals
+
+  def to_h
+    {
+      title: @title,
+      author: @author,
+      rentals: @rentals.map(&:to_h)
+    }
+  end
 end
